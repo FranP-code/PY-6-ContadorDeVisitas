6a4f5ba928bce9d1ba3bf32928b7f868a93e820b
Francisco P <franpessano1@gmail.com>
2021-05-22 20:50:18 -0300

    Base de HTML hecho

---
+++ index.php
@@ -0,0 +1,27 @@
+<?php
+
+require 'logic.php';
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Contador de visitas</title>
+    <link rel="stylesheet" href="style.css">
+    <link rel="stylesheet" href="normalize.css">
+</head>
+<body>
+    <div class="container">
+    <div class="blue-circle">
+        <div class="gray-rectangle">
+            <h2 class="visits">VISITAS</h2>
+            <h2 class="amount"><?php $c ?></h2>
+    </div>
+</div>
+</div>
+</body>
+</html>
